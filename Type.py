bbf94b6da680b0cee841b5558c0ae9f222810f16
Данил Илюхин <danililukhin@yandex.ru>
2022-06-12 23:18:16 +0500

    Code added

--- Type.py
+++ Type.py
@@ -1,1 +1,59 @@
-print('type for me')
+import random
+
+HELP = """
+help — напечатать справку по программе.
+add — добавить задачу в список (название задачи запрашиваем у пользователя).
+show — напечатать все добавленные задачи.
+exit — выйти из программы
+random — добавить случайную задачу на дату Сегодня"""
+
+RANDOM_TASKS = [
+  'Записаться на курс Нетологии', 'Написать Гвидо письмо', 'Покормить кошку', 'Помыть машину'
+]
+
+tasks = {}
+
+# Сегодня, Завтра, 31.12 ...
+# [Задача1, Задача2, Задача3]
+# Дата -> [Задача1, Задача2, Задача3]
+
+run = True
+
+def add_todo(date, task):
+  if date in tasks:
+      # Дата есть в словаре
+      # Добавляем в список задачу
+      tasks[date].append(task)
+  else:
+      # Даты в словаре нет
+      # Создаем запись с ключом date
+      tasks[date] = []
+      tasks[date].append(task)
+  print('Задача', task, 'добавлена на дату', date)
+
+while run:
+  command = input('Введите команду: ')
+  if command == 'help':
+    print(HELP)
+  elif command == 'show':
+    date = input('Введите дату для отображения списка задач: ')
+    if date in tasks:
+      for task in tasks[date]:
+        print('- ', task)
+    else:
+      print('Такой даты нет')
+  elif command == 'add':
+    date = input('Введите дату для добавления задачи: ')
+    task = input('Введите название задачи: ')
+    add_todo(date, task)
+  elif command == 'exit':
+    print('Спасибо за использование! До свидания!')
+    break
+  elif command == 'random':
+    task = random.choice(RANDOM_TASKS)
+    add_todo('Сегодня', task)
+  elif command == 'random_date':
+    add_todo(date, RANDOM_TASKS)
+  else:
+    print('Неизвестная команда')
+    break
